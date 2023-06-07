377849329f6a5e0876ca89ef9ea4b071d20d1090
Darshan G K <122042809+imDarshanGK@users.noreply.github.com>
2023-06-07 08:59:35 +0530

    functions which works on tuple, list, set
    functions which works on tuple, list, set and dict.

---
+++ functions which works on tuple, list, set and dict..py
@@ -0,0 +1,9 @@
+#Built in functions which works on tuple, list, set and dict.
+marks = [41,86,94,74,62,58]
+print('minimum=',min(marks))
+print('maximum=',max(marks))
+print('sum=',sum(marks))
+print('length=',len(marks))
+print('sorted marks=',sorted(marks))
+del(marks)
+print('marks=',marks)
