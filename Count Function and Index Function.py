acdc0f41cef94f54e2d98c2489955ba6482732a9
Darshan G K <122042809+imDarshanGK@users.noreply.github.com>
2023-06-20 17:43:41 +0530

    Count Function and Index Function
    Count Function:  This function gives number of occurrences of gives element.
    Index Function: This function gives index of given values.

---
+++ Count Function and Index Function.py
@@ -0,0 +1,11 @@
+#Count Function: This function gives number of occurrence fo given element.
+
+#Index Function: This function gives index if given value.
+
+a = [20,80,40,20,50,80,20]
+print(a.count(20))
+print(a.count(80))
+print(a.count(40))
+print(a.count(100))
+print(a.index(20))
+print(a.index(80))
